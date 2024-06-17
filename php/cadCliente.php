6c8c6e9e44d3c3c9a6d8202cfdce9f22c58b8891
jvdragon05 <joaovitortbt@gmail.com>
2024-06-16 22:07:57 -0300

    commit

--- php/cadCliente.php
+++ php/cadCliente.php
@@ -18,34 +18,39 @@ function lerImagem($caminhoImagem) {
 }
 
 
-if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
-    $imagemCaminho = $_FILES['imagem']['tmp_name'];
-    $imagemBinaria = lerImagem($imagemCaminho);
-} else {
-    die("Erro ao enviar a imagem.");
-}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
+        $imagemCaminho = $_FILES['imagem']['tmp_name'];
+        $imagemBinaria = lerImagem($imagemCaminho);
+    } else {
+        die("Erro ao enviar a imagem.");
+    }
+
 
     $nome = $_POST["nome"];
     $sobre = $_POST["sobre"];
     $data = $_POST["data"];
     $cpf = $_POST["cpf"];
     $tel = $_POST["telefone"];
-    $cargo = $_POST["cargo"];
-    $salario = $_POST["sal"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
 
-    $insere = "INSERT INTO `usuarios`(`nome`, `sobrenome`, `cpf`, `dataNasc`, `telefone`, `cargo`, `salario`, `email`, `senha`, `foto`, `tipoUsuario`) VALUES ('$nome', '$sobre', '$cpf', '$data', '$tel', '$cargo', '$salario', '$email', '$senha', '$imagemBinaria', 'cliente')";
+
+    $stmt = $conn->prepare("INSERT INTO usuarios (nome, sobrenome, cpf, dataNasc, telefone, email, senha, foto, tipoUsuario) VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'cliente')");
+    $stmt->bind_param("ssssssds", $nome, $sobre, $cpf, $data, $tel, $email, $senha, $imagemBinaria);
 
 
+    if ($stmt->execute()) {
+        header("Location: ../");
+        exit();
+    } else {
+        echo "Erro ao inserir registro: " . $stmt->error;
+    }
 
-if ($conn->query($insere) == TRUE) {
-    header("location: ../");
-} else {
-    echo "Erro ao inserir registro: " . $stmt->error;
+
+    $stmt->close();
 }
-
-
 
 $conn->close();
 ?>
